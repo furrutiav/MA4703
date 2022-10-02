19d6a118c40134eab6ccd846fafcf9324bf36a0f
furrutiav <74210202+furrutiav@users.noreply.github.com>
2022-10-02 19:52:48 -0300

    update results + bocop

---
+++ lab3/lab_3_bocop/build/CMakeFiles/CORE.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Bocop-2.2.1/core/sources/IO/readDefinition.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/IO/readDefinition.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/IO/writeDefinition.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/IO/writeDefinition.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/IO/writeSolution.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/IO/writeSolution.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/interpolation/BocopInterpolation.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/interpolation/BocopInterpolation.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/interpolation/BocopLinearInterpolation.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/interpolation/BocopLinearInterpolation.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/interpolation/BocopSplinesInterpolation.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/interpolation/BocopSplinesInterpolation.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/ipopt/BocopDefinition.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/ipopt/BocopDefinition.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/ipopt/BocopOptimize.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimize.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/ipopt/BocopOptimizeBatch.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimizeBatch.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/ipopt/BocopOptimizeSingle.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimizeSingle.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/BocopProblem.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/BocopProblem.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/Error.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/Error.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/evalNLPBounds.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/evalNLPBounds.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/generateTapes.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/generateTapes.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/overloaded_from_nlp.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/overloaded_from_nlp.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/publicTools.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/publicTools.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/nlp/tools.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/nlp/tools.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/optionalfunctions/postProcessing.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/optionalfunctions/postProcessing.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/optionalfunctions/preProcessing.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/optionalfunctions/preProcessing.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/startingpoint/GenerationVariable.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/startingpoint/GenerationVariable.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/startingpoint/StartFromInitFile.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/startingpoint/StartFromInitFile.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/startingpoint/StartFromPreviousSolution.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/startingpoint/StartFromPreviousSolution.cpp.obj"
+  "C:/Bocop-2.2.1/core/sources/startingpoint/StartingPoint.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/core/sources/startingpoint/StartingPoint.cpp.obj"
+  "C:/Bocop-2.2.1/examples/lab_3_bocop/dependencies.cpp" "C:/Bocop-2.2.1/examples/lab_3_bocop/build/CMakeFiles/CORE.dir/examples/lab_3_bocop/dependencies.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CPPAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../ThirdParty/Ipopt-3.12.12/include/coin"
+  "../../../ThirdParty/Ipopt-3.12.12/include/coin/ThirdParty"
+  "../../../ThirdParty/cppad/include"
+  ".."
+  "../../../core/headers"
+  "../../../core/sources"
+  "../../../core/sources/nlp"
+  "../../../core/sources/optionalfunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab3/lab_3_bocop/build/CMakeFiles/bocop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bocop.dir/core/main.cpp.obj"
+  "../bocop.pdb"
+  "../bocop.exe"
+  "../bocop.exe.manifest"
+  "../libbocop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bocop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
