bd4c4e41ad47abb37a6a15f11aaa1b1011222e06
furrutiav <74210202+furrutiav@users.noreply.github.com>
2022-10-12 17:56:22 -0300

    update results

---
+++ lab4/bocop/problema/build/CMakeFiles/CORE.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CORE.dir/core/sources/IO/readDefinition.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/IO/writeDefinition.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/IO/writeSolution.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/ipopt/BocopDefinition.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimizeBatch.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimize.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/ipopt/BocopOptimizeSingle.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/BocopProblem.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/evalNLPBounds.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/Error.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/generateTapes.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/overloaded_from_nlp.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/publicTools.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/nlp/tools.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/startingpoint/GenerationVariable.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/startingpoint/StartFromInitFile.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/startingpoint/StartFromPreviousSolution.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/startingpoint/StartingPoint.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/interpolation/BocopInterpolation.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/interpolation/BocopLinearInterpolation.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/interpolation/BocopSplinesInterpolation.cpp.obj"
+  "CMakeFiles/CORE.dir/examples/lab4/dependencies.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/optionalfunctions/preProcessing.cpp.obj"
+  "CMakeFiles/CORE.dir/core/sources/optionalfunctions/postProcessing.cpp.obj"
+  "lib/libCORE.pdb"
+  "lib/libCORE.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CORE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab4/bocop/problema/build/CMakeFiles/bocop.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Bocop-2.2.1/core/main.cpp" "C:/Bocop-2.2.1/examples/lab4/build/CMakeFiles/bocop.dir/core/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CPPAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../ThirdParty/Ipopt-3.12.12/include/coin"
+  "../../../ThirdParty/Ipopt-3.12.12/include/coin/ThirdParty"
+  "../../../ThirdParty/cppad/include"
+  ".."
+  "../../../core/headers"
+  "../../../core/sources"
+  "../../../core/sources/nlp"
+  "../../../core/sources/optionalfunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Bocop-2.2.1/examples/lab4/build/CMakeFiles/CORE.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
