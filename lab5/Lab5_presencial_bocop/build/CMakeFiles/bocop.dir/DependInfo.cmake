ee70fffb111b0a0f5fc1326b712bec048e904e73
furrutiav <74210202+furrutiav@users.noreply.github.com>
2022-11-16 17:36:55 -0300

    update lab5

---
+++ lab5/Lab5_presencial_bocop/build/CMakeFiles/bocop.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Bocop-2.2.1/core/main.cpp" "C:/Users/felip/Documents/GitHub/MA4703/lab5/Lab5_presencial_bocop/build/CMakeFiles/bocop.dir/core/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CPPAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Bocop-2.2.1/ThirdParty/Ipopt-3.12.12/include/coin"
+  "C:/Bocop-2.2.1/ThirdParty/Ipopt-3.12.12/include/coin/ThirdParty"
+  "C:/Bocop-2.2.1/ThirdParty/cppad/include"
+  "C:/Bocop-2.2.1/examples/Lab5_1/build/.."
+  "C:/Bocop-2.2.1/core/headers"
+  "C:/Bocop-2.2.1/core/sources"
+  "C:/Bocop-2.2.1/core/sources/nlp"
+  "C:/Bocop-2.2.1/core/sources/optionalfunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/felip/Documents/GitHub/MA4703/lab5/Lab5_presencial_bocop/build/CMakeFiles/CORE.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab5/Lab5_presencial_bocop/build/CMakeFiles/bocop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bocop.dir/core/main.cpp.obj"
+  "C:/Bocop-2.2.1/examples/Lab5_1/bocop.pdb"
+  "C:/Bocop-2.2.1/examples/Lab5_1/bocop.exe"
+  "C:/Bocop-2.2.1/examples/Lab5_1/bocop.exe.manifest"
+  "C:/Bocop-2.2.1/examples/Lab5_1/libbocop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bocop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
